40d976d294e4915068cae6624ab1dd2bd7e88957
Hashan Punchihewa <hashan.punchihewa@gmail.com>
2013-12-04 20:53:12 +0000

    Renamed web.js to app.js

--- app.js
+++ app.js
@@ -1,11 +1,47 @@
-var CS = require("./Streams.js").CS;
-var FS = require("./Streams.js").FS;
-var OS = require("./Streams.js").OS;
-var TS = require("./Streams.js").TS;
+var Program = require("commander");
+var Inquirer = require("inquirer");
+var Database = require("./models/index.common.js").Database;
+var Atrium = require("./app/index/route.common.js").Atrium;
 
-var Stream = TS.get("http://127.0.0.1:5984");
-Stream.pipe(process.stdout);
+Program
+    .version("0.0.1")
+    .option("-p, --port [number]", "Specify Port to Listen At", "4000", parseInt)
+    .option("-c, --create-user", "Create A User Account")
+    .option("-s, --create-subject", "Create A Subject")
+    .parse(process.argv);
 
-Stream.on("error", function(err) {
-    console.log(err);
-});
+
+if(Program.createUser) {
+    Inquirer.prompt([
+	{name: "username", message: "Username: "},
+	{name: "password", message: "Password: ", type: "password"},
+	{name: "role", message: "Role: ", type: "list", choices: ["pupil", "teacher", "admin"]}
+    ], function(data) {
+	Database.user.create({
+	    username: data["username"],
+	    password: data["password"],
+	    role: data["role"]
+	});
+    });
+} else if(Program.createSubject) {
+    Inquirer.prompt([
+	{name: "name", message: "Name: "},
+	{name: "subjectName", message: "Subject Name: "}
+    ], function(data) {
+	Database.subject.create({
+	    name: data["name"],
+	    subject_name: data["subjectName"],
+	    blog: [],
+	    vocab_quizzes: []
+	});
+    });
+} else {
+    Atrium.listen(Program.port, {
+	favicon: true,
+	logger: true,
+	open: "app",
+	session: {
+	    secret: "uber seecret"
+	}
+    });
+}
